92c2d9163543c01200a06df66a2abdac4c71cf9b
Angel Leobardo Perez Perez <angelperez7246@gmail.com>
2025-10-14 10:55:12 -0600

    Agrego API de loginMongo

---
+++ LaTroca.Application/DTOs/RegisterRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorneoUniversitario.Application.DTOs
+{
+    public record RegisterRequest
+    {
+        public string Nombre { get; init; } = string.Empty; // name
+        public string Email { get; init; } = string.Empty;
+        public string Password { get; init; } = string.Empty;
+        public string Rol { get; init; } = string.Empty;
+        public string Bio { get; init; } = string.Empty;
+
+    }
+}
---
+++ LaTroca.Infrastructure/Data/MongoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TorneoUniversitario.Domain.Entities;
+
+namespace LaTroca.Infrastructure.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MongoDbConnection");
+            var databaseName = configuration["DatabaseSettings:DatabaseName"];
+
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<Usuario> Usuarios => _database.GetCollection<Usuario>("users");
+    }
+}
